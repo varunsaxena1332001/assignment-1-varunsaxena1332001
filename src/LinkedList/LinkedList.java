095ec93dc0b186e3bc5d716fe5d46d218348d488
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-02-23 21:05:58 +0530

    LinkedList<E> class is formed which is implementing LinkedListADT<E>

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+import adt.LinkedListADT;
+
+public class LinkedList<E> implements LinkedListADT {
+    @Override
+    public void add(Object firstName, Object lastName, Object number, Object email) {
+
+    }
+
+    @Override
+    public Object remove(Object firstName) {
+        return null;
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public int search(Object firstName) {
+        return 0;
+    }
+}
