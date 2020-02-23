2a08aeace378b517d4513e943bc1f9c823ab2b19
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-02-23 21:02:54 +0530

    LinkedListADT<E> is formed containing four methods add(),remove(), print(),search()

--- src/adt/LinkedListADT.java
+++ src/adt/LinkedListADT.java
@@ -1,5 +1,11 @@
 package adt;
 
 public interface LinkedListADT<E> {
+    public void add(E firstName, E lastName, E number, E email);
 
+    public E remove(E firstName);
+
+    public void print();
+
+    public int search(E firstName);
 }
