1eed3a7eccf76b43e0dfa3a9146f95ca085779ac
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-02-23 21:36:16 +0530

    create addContact() method to add contact details in list

--- src/Person/Person.java
+++ src/Person/Person.java
@@ -1,4 +1,20 @@
 package Person;
 
+import LinkedList.LinkedList;
+
 public class Person {
+    public String firstName;
+    public String lastName;
+    public String number;
+    public String email;
+    LinkedList<String> linkedList = new LinkedList<String>();
+
+    public void addContact(String firstName, String lastName, String number, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.number = number;
+        this.email = email;
+        linkedList.add(firstName, lastName, number, email);
+        linkedList.sort();
+    }
 }
